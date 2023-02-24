f8da51a5e611445793699c063bb0a314bdce921f
YaodongLiu <ttttttliu@qq.com>
2023-02-24 16:25:19 +0800

    初始化

---
+++ src/main/java/com/bocd/mkt/gatewaysentinelsample/GatewaySentinelSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bocd.mkt.gatewaysentinelsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySentinelSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySentinelSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bocd/mkt/gatewaysentinelsample/GatewaySentinelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bocd.mkt.gatewaysentinelsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySentinelSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
