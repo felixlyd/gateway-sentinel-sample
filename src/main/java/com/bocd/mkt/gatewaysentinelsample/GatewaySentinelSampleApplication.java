b8d8dc25e5d34061fae2b73db91abd30f7f74068
YaodongLiu <ttttttliu@qq.com>
2023-03-15 18:01:28 +0800

    2.6.11不支持sentinel在spring cloud中的配置项，2.6.3支持

--- src/main/java/com/bocd/mkt/gatewaysentinelsample/GatewaySentinelSampleApplication.java
+++ src/main/java/com/bocd/mkt/gatewaysentinelsample/GatewaySentinelSampleApplication.java
@@ -1,9 +1,14 @@
 package com.bocd.mkt.gatewaysentinelsample;
 
+import com.alibaba.cloud.sentinel.SentinelProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SentinelProperties.class)
+@ConfigurationPropertiesScan
 public class GatewaySentinelSampleApplication {
 
     public static void main(String[] args) {
